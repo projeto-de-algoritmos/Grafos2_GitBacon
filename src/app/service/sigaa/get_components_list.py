49aaa9f35fafe2b21605f4d2e43033c1673cc470
Nicolas Souza <nszchagas@gmail.com>
2023-05-09 12:24:41 -0300

    Incluindo busca pelas matérias bloqueadas por cada matéria no script.

--- src/app/service/sigaa/get_components_list.py
+++ src/app/service/sigaa/get_components_list.py
@@ -1,67 +1,121 @@
-import requests
+import urllib.parse
+from typing import List
+
+import json
+import re
+import ast
 
 from requests import Session
 from bs4 import BeautifulSoup
-
-s = Session()
 
 
 class Materia:
     cod: str
     nome: str
     ch: int
-    info: any
+    requirements_post_data: dict
+    post_requirements: List
+    post_requirements_query: str
 
-    def __init__(self, cod, nome, ch, info):
+    def __init__(self, cod, nome, ch, component_id, requirements_post_data):
         self.cod = cod
         self.nome = nome
         self.ch = int(ch.replace('h', ''))
-        self.info = info
+        self.post_requirements = []
+        self.component_id = component_id
 
-    def __str__(self):
-        return '{' + f'"cod" :  "{self.cod}",\n"nome" : "{self.nome}",\n"ch" :  {self.ch},\n"info" : "{self.info}"' + '\n}\n'
+        requirements_post_data = ast.literal_eval(requirements_post_data)
+        requirements_post_data['formListagemComponentes'] = 'formListagemComponentes'
+        requirements_post_data['javax.faces.ViewState'] = 'j_id2'
+        requirements_post_data['id'] = int(component_id)
+        self.post_requirements_query = urllib.parse.urlencode(requirements_post_data)
+
+    def set_post_requirements(self, requirements):
+        self.post_requirements = requirements
 
 
-cod_unidades = 0  # FGA = 673, 0 para buscar todas.
+cod_unidades = 0  # FGA = 673, 0 para buscar todas, MAT 518
 
 sigaa_url: str = 'https://sigaa.unb.br/sigaa/public/componentes/busca_componentes.jsf'
-data: dict() = {
+#
+header = {'Content-Type': 'application/x-www-form-urlencoded',
+          'Referer': sigaa_url,
+          'Origin': 'https://sigaa.unb.br',
+          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.0.0'}
+data: dict = {
     'form': 'form',
     'form:nivel': 'G',
-    'form:tipo': 0,
+    'form:checkTipo': 'on',
+    'form:tipo': 2,
     'form:j_id_jsp_190531263_11': None,
     'form:j_id_jsp_190531263_13': None,
+    # 'form:checkUnidade': 'on',
     'form:unidades': cod_unidades,
     'form:btnBuscarComponentes': 'Buscar Componentes',
     'javax.faces.ViewState': 'j_id1'
 }
+
+s = Session()
 s.get(sigaa_url)
-r = s.post(sigaa_url, data=data, headers={
-    'Content-Type': 'application/x-www-form-urlencoded'})
-print(f'Request to {sigaa_url} returned status code: {r.status_code}.')
+r = s.post(sigaa_url, data=data, headers=header)
+print(f'Request to {r.url} returned status code: {r.status_code}.')
 print('Parsing content...')
 
-soup = BeautifulSoup(r.content, 'html.parser')
-components = []
+soup = BeautifulSoup(r.content, 'html.parser', from_encoding='UTF-8')
+components: List[Materia] = []
 
 table_listagem = soup.select('table.listagem')[0]
 
 for linha in table_listagem.select('tr'):
+    component_id = ''
+    post_data = ''
     for a in linha.select('a'):
         if a['title']:
             if 'Detalhes' in a['title']:
                 component_info = a['onclick']
+                #
+                post_data_pattern = re.compile("\{\'.*\'\}")
+                post_data = post_data_pattern.findall(component_info)[0]
+                pattern = re.compile("'id':'[\\d]*'")
+                component_id = int(pattern.findall(component_info)[0].replace("'", '').split(':')[-1])
     dados = []
     for td in linha.select('td'):
         dados.append(td.text.replace('\n', ''))
-    if (dados):
+    if dados:
         components.append(
-            Materia(cod=dados[0], nome=dados[1], ch=dados[3], info=component_info))
+            Materia(
+                cod=dados[0],
+                nome=dados[1],
+                ch=dados[3],
+                component_id=component_id,
+                requirements_post_data=post_data
+            )
+        )
 
+for c in components:
+    print(
+        f"Getting post-requirements for component: {c.cod} {c.nome}")
 
-with open('materias.json', 'w') as f:
-    f.write('{ "materias": [\n')
-    f.write(components[-1].__str__())
-    for component in components[1:]:
-        f.write(',\n' + component.__str__())
-    f.write('\n]}')
+    r = s.get(f'{sigaa_url}?{c.post_requirements_query}',
+              headers=header,
+              allow_redirects=False
+              )
+    print(f'\t > Request to {sigaa_url} returned status code: {r.status_code}')
+
+    requirements_soup = BeautifulSoup(r.content, 'html.parser', from_encoding='UTF-8')
+    regex_match = re.compile('pré-requisito(.*?)Histórico').findall(requirements_soup.text.replace('\n', ' '))
+
+    if regex_match:
+        requirements = regex_match[0].replace(
+            'pré-requisito', '').replace('Histórico', '')
+        print(f"\t\t > {c.cod} - {c.nome} blocks: {requirements.strip().split('   ')}")
+        c.set_post_requirements(requirements.strip().split('   '))
+
+write_data = []
+for c in components:
+    c_dict = c.__dict__
+    c_dict.pop('post_requirements_query')
+    write_data.append(c.__dict__)
+
+with open('materias.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(write_data, indent=2))
